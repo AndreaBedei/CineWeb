777c186c0905da3fc61af7ceaabef4963e4234ce
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-12-04 17:01:47 +0100

    Insert tailwind

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@ import HelloWorld from './components/HelloWorld.vue'
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
+    <h1 class="text-green-600">AAAAAAAAAAA</h1>
+
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
