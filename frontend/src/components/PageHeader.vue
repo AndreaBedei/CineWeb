50b93e4cf6f7f00db4c4c484f268fd49c23bcb21
LeoGiaco <leogiaco2@gmail.com>
2024-12-07 18:16:31 +0100

    Continue with header

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -6,9 +6,11 @@ defineProps<{
 
 <template>
     <div class="flex gap-4 p-2 border-b-solid border-b-black border-b-1">
-        <img src="../assets/img/icon.svg" alt="Icona CineWeb" class="w-10 h-10">
-        <input type="text" name="searchbar" id="searchbar" placeholder="Ricerca:" class="flex-grow">
-        <button v-if="logged">Profilo Utente</button>
-        <button>{{ logged ? "Logout" : "Login" }}</button>
+        <img src="../assets/img/icon.svg" alt="Icona CineWeb" class="w-12 h-12">
+        <input type="text" name="searchbar" id="searchbar" placeholder="Ricerca:" class="w-28 sm:flex-grow border-slate-400 border-1 px-2">
+        <div class="flex gap-4 ms-auto">
+            <button v-if="logged">Profilo Utente</button>
+            <button>{{ logged ? "Logout" : "Login" }}</button>
+        </div>
     </div>
 </template>
