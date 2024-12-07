41cd27b950ab1998b7955e94333f7cda560458eb
LeoGiaco <leogiaco2@gmail.com>
2024-12-07 17:55:02 +0100

    Make icon an svg

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -6,7 +6,7 @@ defineProps<{
 
 <template>
     <div class="flex gap-4 p-2 border-b-solid border-b-black border-b-1">
-        <div class="w-6 h-6 bg-red-300"></div>
+        <img src="../assets/img/icon.svg" alt="Icona CineWeb" class="w-10 h-10">
         <input type="text" name="searchbar" id="searchbar" placeholder="Ricerca:" class="flex-grow">
         <button v-if="logged">Profilo Utente</button>
         <button>{{ logged ? "Logout" : "Login" }}</button>
