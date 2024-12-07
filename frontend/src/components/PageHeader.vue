b7bbc4fd7fe787f3a47ed28bfe1f19f16b3b56d6
LeoGiaco <leogiaco2@gmail.com>
2024-12-07 18:33:22 +0100

    More adjustments to header

--- frontend/src/components/PageHeader.vue
+++ frontend/src/components/PageHeader.vue
@@ -6,11 +6,11 @@ defineProps<{
 
 <template>
     <div class="flex gap-4 p-2 border-b-solid border-b-black border-b-1">
-        <img src="../assets/img/icon.svg" alt="Icona CineWeb" class="w-12 h-12">
-        <input type="text" name="searchbar" id="searchbar" placeholder="Ricerca:" class="w-28 sm:flex-grow border-slate-400 border-1 px-2">
+        <img src="../assets/img/icon.svg" alt="Icona CineWeb" class="size-10 sm:size-12">
+        <input type="text" name="searchbar" id="searchbar" placeholder="Ricerca:" class="w-0 flex-grow border-slate-400 border-1 px-2 text-sm sm:text-base">
         <div class="flex gap-4 ms-auto">
-            <button v-if="logged">Profilo Utente</button>
-            <button>{{ logged ? "Logout" : "Login" }}</button>
+            <button v-if="logged" class="w-14 text-sm sm:w-fit sm:text-base">Profilo Utente</button>
+            <button class="text-sm sm:text-base">{{ logged ? "Logout" : "Login" }}</button>
         </div>
     </div>
 </template>
