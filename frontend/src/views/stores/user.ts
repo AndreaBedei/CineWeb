d1a46ce685132fe8d045cd623c3ae2796e7a2359
LeoGiaco <leogiaco2@gmail.com>
2024-12-12 20:11:46 +0100

    Rewrite user store using refs

--- frontend/src/views/stores/user.ts
+++ frontend/src/views/stores/user.ts
@@ -1,77 +1,73 @@
 import axios, { AxiosError } from 'axios';
 import { defineStore } from 'pinia'
 import CryptoJS from 'crypto-js';
+import { ref } from 'vue';
 
 interface Interest {
     _id: string;
     name: string;
 }
 
-async function loadData(userId: string) {
-    const user = useUserStore()
+export const useUserStore = defineStore('user', () => {
+    const userId = ref("");
+    const username = ref("");
+    const email = ref("");
+    const name = ref("");
+    const surname = ref("");
+    const profileImg = ref("");
+    const interests = ref([] as Interest[]);
 
-    const response = await axios.get(`http://localhost:3001/users/${userId}`);
-    const data = response.data;
-    if (data) {
-        user.email = data.email;
-        user.name = data.name;
-        user.surname = data.surname;
-        user.interests = data.favoriteGenres;
-        user.profileImg = data.profilePicture;
+    async function _loadData(userId: string) {
+        const response = await axios.get(`http://localhost:3001/users/${userId}`);
+        const data = response.data;
+        if (data) {
+            email.value = data.email;
+            name.value = data.name;
+            surname.value = data.surname;
+            interests.value = data.favoriteGenres;
+            profileImg.value = data.profilePicture;
+        }
     }
-}
 
-function hashPassword(password: string, salt: string): string {
-    const saltedPassword = password + salt;
-    return CryptoJS.SHA512(saltedPassword).toString(CryptoJS.enc.Hex);
-}
+    function _hashPassword(password: string, salt: string): string {
+        const saltedPassword = password + salt;
+        return CryptoJS.SHA512(saltedPassword).toString(CryptoJS.enc.Hex);
+    }
 
-export const useUserStore = defineStore('user', {
-    state: () => {
-        return {
-            userId: "",
-            username: "",
-            email: "",
-            name: "",
-            surname: "",
-            profileImg: "",
-            interests: [] as Interest[]
-        }
-    },
-    // could also be defined as
-    // state: () => ({ count: 0 })
-    actions: {
-        async login(email: string, password: string): Promise<{ success: true } | { success: false, errorNum: number, error: AxiosError | undefined }> {
-            try {
+    async function login(email: string, password: string): Promise<{ success: true } | { success: false, errorNum: number, error: AxiosError | undefined }> {
+        try {
 
-                const emailCheck = await axios.get('http://localhost:3001/users/email?email=' + email);
-                if (emailCheck.data.length === 0) {
-                    return { success: false, errorNum: 0, error: undefined };
-                }
-                const salt = emailCheck.data[0].salt;
-                const hashPassw = hashPassword(password, salt);
-                const ret = await axios.post('http://localhost:3001/users/email?email=' + email, { password: hashPassw });
-
-                loadData(ret.data);
-                this.userId = ret.data;
+            const emailCheck = await axios.get('http://localhost:3001/users/email?email=' + email);
+            if (emailCheck.data.length === 0) {
+                return { success: false, errorNum: 0, error: undefined };
             }
-            catch (error) {
-                console.error(error);
-                return { success: false, errorNum: 1, error: error as AxiosError };
-            }
-            return { success: true };
-        },
-        logout() {
-            this.userId = "";
-            this.username = "";
-            this.email = "";
-            this.name = "";
-            this.surname = "";
-            this.profileImg = "";
-            this.interests = [];
-        },
-        async refresh() {
-            await loadData(this.userId);
+            const salt = emailCheck.data[0].salt;
+            const hashPassw = _hashPassword(password, salt);
+            const ret = await axios.post('http://localhost:3001/users/email?email=' + email, { password: hashPassw });
+
+            _loadData(ret.data);
+            userId.value = ret.data;
         }
-    },
+        catch (error) {
+            console.error(error);
+            return { success: false, errorNum: 1, error: error as AxiosError };
+        }
+        return { success: true };
+    }
+
+    function logout() {
+        userId.value = "";
+        username.value = "";
+        email.value = "";
+        name.value = "";
+        surname.value = "";
+        profileImg.value = "";
+        interests.value = [];
+    }
+
+    async function refresh() {
+        await _loadData(userId.value);
+    }
+
+    return { userId, username, email, name, surname, profileImg, interests, login, logout, refresh }
 })
