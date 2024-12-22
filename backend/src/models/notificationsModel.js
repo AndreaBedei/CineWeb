b97f40614897f6b8b098906386bf5c48ed628860
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-12-22 21:04:27 +0100

    Added timestamp field to notifications schema

--- backend/src/models/notificationsModel.js
+++ backend/src/models/notificationsModel.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const notificationSchema = new mongoose.Schema({
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     text: { type: String, required: true },
-    resource: { type: mongoose.Schema.Types.ObjectId, required: true } 
+    resource: { type: mongoose.Schema.Types.ObjectId, required: true } ,
+    timestamp: { type: Date, default: Date.now }
 });
 
 const notificationsModel = mongoose.model('Notification', notificationSchema);
