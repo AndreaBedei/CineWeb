cc83047b285c8e81403da7c2536dfe98047b8d47
Andrea Bedei <andrea.bedei2@studio.unibo.it>
2024-12-24 16:56:40 +0100

    Configure Vite server settings for improved accessibility and port management

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
@@ -12,9 +11,16 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
+  },
+
+  server: {
+    host: '0.0.0.0', // Rende il server accessibile da qualsiasi indirizzo IP
+    port: 3000,      // Puoi cambiare la porta se necessario
+    strictPort: true, // Fallisce se la porta Ã¨ giÃ  occupata
   },
 })
