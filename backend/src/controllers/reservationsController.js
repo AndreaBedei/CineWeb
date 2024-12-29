6afc9bc3b5a3cc78288160598372a685c23d1f67
Fabio Notaro <fabio.notaro2@studio.unibo.it>
2024-12-29 20:25:38 +0100

    Completed reservations

--- backend/src/controllers/reservationsController.js
+++ backend/src/controllers/reservationsController.js
@@ -1,4 +1,6 @@
 const { reservationsModel } = require('../models/reservationsModel');
+const { screeningsModel } = require('../models/screeningsModel');
+const { seatsModel } = require('../models/seatsModel');
 const mongoose = require('mongoose');
 
 exports.reservationsList = (req, res) => {
@@ -60,16 +62,61 @@ exports.getReservationByID = (req, res) => {
         });
 };
 
-exports.createReservation = (req, res) => {
-    const reservation = new reservationsModel(req.body);
-    reservation.save()
-        .then(doc => {
-            res.json(doc);
-        })
-        .catch(err => {
-            res.status(500).send(err);
+exports.createReservation = async (req, res) => {
+    const { screeningId, seats, userId, price } = req.body;
+    console.log(req.body);
+
+    if (!screeningId || !seats || !userId || !price) {
+        return res.status(400).send('Missing required fields');
+    }
+
+    console.log(screeningId, seats, userId, price);
+    try {
+        // Step 1: Find the cinema hall ID associated with the screening
+        const screening = await screeningsModel.findById(screeningId).populate('cinemaHall');
+        if (!screening) {
+            return res.status(404).send('Screening not found');
+        }
+        console.log("ho fatto gli screening");
+        const cinemaHallId = screening.cinemaHall._id;
+
+        // Step 2: Iterate through the seats array to find seat IDs
+        const seatIds = [];
+        for (const seat of seats) {
+            const { row, col } = seat;
+            console.log(row, col, cinemaHallId);
+            const seatDoc = await seatsModel.findOne({
+                cinemaHall: cinemaHallId,
+                row: row,
+                column: col,
+                isAvailable: true
+            });
+
+            if (!seatDoc) {
+                return res.status(400).send(`Seat at row ${row}, column ${col} does not exist.`);
+            }
+
+            seatIds.push(seatDoc._id);
+        }
+        console.log("ho fatto anche tutti i seats");
+
+        // Step 3: Create the reservation
+        const newReservation = new reservationsModel({
+            user: userId,
+            screening: screeningId,
+            seats: seatIds,
+            totalPaid: price,
         });
-}
+
+        await newReservation.save();
+
+        res.status(201).json(newReservation);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while creating the reservation');
+    }
+};
+
 
 exports.updateReservation = (req, res) => {
     reservationsModel.findByIdAndUpdate(req.params.id, req.body, { new: true })
